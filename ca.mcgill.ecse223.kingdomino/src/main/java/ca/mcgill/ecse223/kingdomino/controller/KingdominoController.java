1cb2f1a4afc845290c58ec7a159dc530cd8bc681
ZeroLifeCat <mandylogan8x@gmail.com>
2020-02-24 03:44:12 +0800

    Generated Code from Umple. Added preliminary query method

---
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/KingdominoController.java
@@ -0,0 +1,47 @@
+package ca.mcgill.ecse223.kingdomino.controller;
+
+import ca.mcgill.ecse223.kingdomino.model.Kingdomino;
+
+public class KingdominoController {
+    public KingdominoController(){
+
+    }
+
+    public void createBonusOption(){
+
+    }
+    public void createUser(String aName, Kingdomino aKingdomino){
+
+    }
+
+    public void createGame(){
+
+    }
+
+    public void createPlayer(){
+
+    }
+
+    public void createDomino(){
+
+    }
+    public void createDraft(){
+
+    }
+
+    public void createDominoKingdom(){
+
+    }
+
+    public void createCastle(){
+
+    }
+
+    public void createProperty(){
+
+    }
+
+    public void createDominoSelection(){
+
+    }
+}
