c8e4e6bd16bcc0ed3e891133e0d19a3411317850
violetwei <zhenzhou.wei@mail.mcgill.ca>
2020-02-28 22:56:34 -0500

    Add controllers

--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/KingdominoApplication.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/KingdominoApplication.java
@@ -7,7 +7,7 @@ public class KingdominoApplication {
 	private static Kingdomino kingdomino;
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Welcome to Kingdomino Game!");
 	}
 
 	public static Kingdomino getKingdomino() {
