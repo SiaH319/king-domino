b5068e4c7c413d45c508d62edef63488a51f0410
ZeroLifeCat <mandylogan8x@gmail.com>
2020-03-04 18:49:10 +0800

    Crawled to Midpoint for F15

--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/VerificationController.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/VerificationController.java
@@ -2,13 +2,14 @@ package ca.mcgill.ecse223.kingdomino.controller;
 
 import ca.mcgill.ecse223.kingdomino.model.Castle;
 import ca.mcgill.ecse223.kingdomino.model.DominoInKingdom;
+import ca.mcgill.ecse223.kingdomino.model.TerrainType;
 
 /**
  * @author Violet, Cecilia
  */
 public class VerificationController {
 
-    public static Boolean verifyCastleAjacency (Castle castle, DominoInKingdom domino) {
+    public static boolean verifyCastleAjacency (Castle castle, DominoInKingdom domino) {
         if(castle == null)
             throw new java.lang.IllegalArgumentException("Castle is not created");
         if(domino == null)
@@ -20,11 +21,40 @@ public class VerificationController {
         int x_right = pos_right[0];
         int y_right = pos_right[1];
 
-        return (isAdjacent(castle.getX(), castle.getY(), x_left, y_left) && noOverlappingWithCastle(castle, x_right, y_right))
-                || (isAdjacent(castle.getX(), castle.getY(), x_right, y_right) && noOverlappingWithCastle(castle, x_left, y_left));
+        return (isAdjacentToCastle(castle.getX(), castle.getY(), x_left, y_left) && noOverlappingWithCastle(castle, x_right, y_right))
+                || (isAdjacentToCastle(castle.getX(), castle.getY(), x_right, y_right) && noOverlappingWithCastle(castle, x_left, y_left));
+    }
+    public static boolean verifyNeighborAdjacency(Square[] grid, DominoInKingdom domino) {
+        int x_left = domino.getX();
+        int y_left = domino.getY();
+
+        int[] pos_right = DominoInKingdom.getRightTilePosition(x_left, y_left, domino.getDirection());
+        int x_right = pos_right[0];
+        int y_right = pos_right[1];
+
+        int[][] adjacentXYList = {{0,1},{0,-1},{1,0},{-1,0}};
+        boolean result = false;
+        for (int i = 0;i < 4; i++) {
+            int x_a = x_left + adjacentXYList[i][0];
+            int y_a = x_left + adjacentXYList[i][0];
+            if(!(x_a==x_right && y_a == y_right)){
+                int index = Square.convertPositionToInt(x_a, y_a);
+                result = result || isAdjacenttoGridSquare(grid[index], domino.getDomino().getLeftTile());
+            }
+        }
+
+        for (int i = 0;i < 4; i++) {
+            int x_a = x_right + adjacentXYList[i][0];
+            int y_a = x_right + adjacentXYList[i][0];
+            if(!(x_a==x_left && y_a == y_left)){
+                int index = Square.convertPositionToInt(x_a, y_a);
+                result = result || isAdjacenttoGridSquare(grid[index], domino.getDomino().getRightTile());
+            }
+        }
+        return result;
     }
 
-    public static Boolean verifyNoOverlapping (Castle castle, Square[] grid, DominoInKingdom domino) {
+    public static boolean verifyNoOverlapping (Castle castle, Square[] grid, DominoInKingdom domino) {
         int x_left = domino.getX();
         int y_left = domino.getY();
 
@@ -36,15 +66,20 @@ public class VerificationController {
                 && noOverlappingWithDomino(grid, x_right, y_right) &&  noOverlappingWithDomino(grid, x_left, y_left);
     }
 
-    private static Boolean isAdjacent(int x1, int y1, int x2, int y2){
+    //TODO: 所以老宅的位置和新来的domino贴着的话是算还是不算
+    private static boolean isAdjacenttoGridSquare(Square square, TerrainType dominoTerrainType) {
+        return square.getTerrain()==dominoTerrainType;
+    }
+
+    private static boolean isAdjacentToCastle(int x1, int y1, int x2, int y2){
         return (x1 == x2 && Math.abs(y1 - y2) == 1)||(y1 == y2 && Math.abs(x1 - x2) == 1);
     }
 
-    private static Boolean noOverlappingWithCastle (Castle castle, int x, int y) {
+    private static boolean noOverlappingWithCastle (Castle castle, int x, int y) {
         return !((x == castle.getX()) && (y == castle.getY()));
     }
 
-    private static Boolean noOverlappingWithDomino (Square[] grid, int x, int y) {
+    private static boolean noOverlappingWithDomino (Square[] grid, int x, int y) {
         return grid[Square.convertPositionToInt(x,y)].getTerrain() == null;
     }
 }
---
+++ ca.mcgill.ecse223.kingdomino/src/test/java/ca/mcgill/ecse223/kingdomino/features/VerifyNeighborAdjacencyStepDefinition.java
@@ -0,0 +1,141 @@
+package ca.mcgill.ecse223.kingdomino.features;
+import static org.junit.Assert.assertEquals;
+
+import ca.mcgill.ecse223.kingdomino.KingdominoApplication;
+import ca.mcgill.ecse223.kingdomino.controller.Square;
+import ca.mcgill.ecse223.kingdomino.model.*;
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class VerifyNeighborAdjacencyStepDefinition {
+    private Boolean isValid;
+    private Square[] grid;
+    @Given("the game is initialized for neighbor adjacency")
+    public void initialize_the_game(){
+        // Intialize empty game
+        Kingdomino kingdomino = KingdominoApplication.getKingdomino();
+        Game game = new Game(48, kingdomino);
+        game.setNumberOfPlayers(4);
+        kingdomino.setCurrentGame(game);
+        // Populate game
+        addDefaultUsersAndPlayers(game);
+        createAllDominoes(game);
+        game.setNextPlayer(game.getPlayer(0));
+        KingdominoApplication.setKingdomino(kingdomino);
+    }
+
+
+    @When("check current preplaced domino adjacency is initiated")
+    public void trigger_check_neighbor_adjacency(){
+
+    }
+
+    @Then("the current-domino\\/existing-domino adjacency is {string}")
+    public void the_castle_domino_adjacency_is(String result) {
+        Boolean expectedResult = (!result.equals("invalid"));
+        assertEquals(expectedResult,isValid);
+    }
+
+    @After
+    public void tearDown() {
+        Kingdomino kingdomino = KingdominoApplication.getKingdomino();
+        kingdomino.delete();
+    }
+    ///////////////////////////////////////
+    /// -----Private Helper Methods---- ///
+    ///////////////////////////////////////
+
+    private void addDefaultUsersAndPlayers(Game game) {
+        String[] userNames = { "User1", "User2", "User3", "User4" };
+        for (int i = 0; i < userNames.length; i++) {
+            User user = game.getKingdomino().addUser(userNames[i]);
+            Player player = new Player(game);
+            player.setUser(user);
+            player.setColor(Player.PlayerColor.values()[i]);
+            Kingdom kingdom = new Kingdom(player);
+            new Castle(0, 0, kingdom, player);
+        }
+    }
+    private Domino getdominoByID(int id) {
+        Game game = KingdominoApplication.getKingdomino().getCurrentGame();
+        for (Domino domino : game.getAllDominos()) {
+            if (domino.getId() == id) {
+                return domino;
+            }
+        }
+        throw new java.lang.IllegalArgumentException("Domino with ID " + id + " not found.");
+    }
+    private void createAllDominoes(Game game) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/alldominoes.dat"));
+            String line = "";
+            String delimiters = "[:\\+()]";
+            while ((line = br.readLine()) != null) {
+                String[] dominoString = line.split(delimiters); // {id, leftTerrain, rightTerrain, crowns}
+                int dominoId = Integer.decode(dominoString[0]);
+                TerrainType leftTerrain = getTerrainType(dominoString[1]);
+                TerrainType rightTerrain = getTerrainType(dominoString[2]);
+                int numCrown = 0;
+                if (dominoString.length > 3) {
+                    numCrown = Integer.decode(dominoString[3]);
+                }
+                new Domino(dominoId, leftTerrain, rightTerrain, numCrown, game);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new java.lang.IllegalArgumentException(
+                    "Error occured while trying to read alldominoes.dat: " + e.getMessage());
+        }
+    }
+
+    private TerrainType getTerrainType(String terrain) {
+        switch (terrain) {
+            case "W":
+                return TerrainType.WheatField;
+            case "F":
+                return TerrainType.Forest;
+            case "M":
+                return TerrainType.Mountain;
+            case "G":
+                return TerrainType.Grass;
+            case "S":
+                return TerrainType.Swamp;
+            case "L":
+                return TerrainType.Lake;
+            default:
+                throw new java.lang.IllegalArgumentException("Invalid terrain type: " + terrain);
+        }
+    }
+
+    private DominoInKingdom.DirectionKind getDirection(String dir) {
+        switch (dir) {
+            case "up":
+                return DominoInKingdom.DirectionKind.Up;
+            case "down":
+                return DominoInKingdom.DirectionKind.Down;
+            case "left":
+                return DominoInKingdom.DirectionKind.Left;
+            case "right":
+                return DominoInKingdom.DirectionKind.Right;
+            default:
+                throw new java.lang.IllegalArgumentException("Invalid direction: " + dir);
+        }
+    }
+
+    private Castle getCastle (Kingdom kingdom) {
+        for(KingdomTerritory territory: kingdom.getTerritories()){
+            if(territory instanceof Castle )
+                return (Castle)territory;
+        }
+        return null;
+    }
+}
