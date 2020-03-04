c451af47ae09624e4f75e8d9bbc0398fb9f22ff3
ZeroLifeCat <mandylogan8x@gmail.com>
2020-03-04 17:35:16 +0800

    Completed Feature 14, 17

---
+++ ca.mcgill.ecse223.kingdomino/src/test/java/ca/mcgill/ecse223/kingdomino/features/VerifyCastleAdjacencyStepDefinition.java
@@ -0,0 +1,162 @@
+package ca.mcgill.ecse223.kingdomino.features;
+
+import static org.junit.Assert.assertEquals;
+
+import ca.mcgill.ecse223.kingdomino.KingdominoApplication;
+import ca.mcgill.ecse223.kingdomino.controller.VerificationController;
+import ca.mcgill.ecse223.kingdomino.model.*;
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class VerifyCastleAdjacencyStepDefinition {
+    private Boolean isValid;
+
+    @Given("the game is initialized for castle adjacency")
+    public void given_game_is_initialized_for_castle_adjacency () {
+        // Intialize empty game
+        Kingdomino kingdomino = KingdominoApplication.getKingdomino();
+        Game game = new Game(48, kingdomino);
+        game.setNumberOfPlayers(4);
+        kingdomino.setCurrentGame(game);
+        // Populate game
+        addDefaultUsersAndPlayers(game);
+        createAllDominoes(game);
+        game.setNextPlayer(game.getPlayer(0));
+        KingdominoApplication.setKingdomino(kingdomino);
+    }
+
+    @Given("the current player preplaced the domino with ID {int} at position {int}:{int} and direction {string}")
+    public void the_current_player_preplaced_the_domino_with_ID_at_position_and_direction(Integer int1, Integer int2, Integer int3, String string) {
+        Kingdomino kingdomino = KingdominoApplication.getKingdomino();
+        Game game = kingdomino.getCurrentGame();
+        Domino domino = getdominoByID(int1);
+        DominoInKingdom  dominoInKingdom = new DominoInKingdom(int2, int3, game.getNextPlayer().getKingdom(),domino);
+        dominoInKingdom.setDirection(getDirection(string));
+    }
+
+    @When("check castle adjacency is initiated")
+    public void application_check_CastleAdjacency () {
+        Kingdomino kingdomino = KingdominoApplication.getKingdomino();
+        Game game = kingdomino.getCurrentGame();
+        Player player = game.getNextPlayer();
+        Castle castle = getCastle(player.getKingdom());
+        List<KingdomTerritory> list= player.getKingdom().getTerritories();
+        DominoInKingdom dominoInKingdom = (DominoInKingdom)list.get(list.size() - 1);
+
+        try {
+            isValid = VerificationController.verifyCastleAjacency(castle, dominoInKingdom);
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Then("the castle\\/domino adjacency is {string}")
+    public void the_castle_domino_adjacency_is(String result) {
+        Boolean expectedResult = (!result.equals("invalid"));
+        assertEquals(expectedResult,isValid);
+    }
+
+    @After
+    public void tearDown() {
+        Kingdomino kingdomino = KingdominoApplication.getKingdomino();
+        kingdomino.delete();
+    }
+
+    ///////////////////////////////////////
+    /// -----Private Helper Methods---- ///
+    ///////////////////////////////////////
+
+    private void addDefaultUsersAndPlayers(Game game) {
+        String[] userNames = { "User1", "User2", "User3", "User4" };
+        for (int i = 0; i < userNames.length; i++) {
+            User user = game.getKingdomino().addUser(userNames[i]);
+            Player player = new Player(game);
+            player.setUser(user);
+            player.setColor(Player.PlayerColor.values()[i]);
+            Kingdom kingdom = new Kingdom(player);
+            new Castle(0, 0, kingdom, player);
+        }
+    }
+    private Domino getdominoByID(int id) {
+        Game game = KingdominoApplication.getKingdomino().getCurrentGame();
+        for (Domino domino : game.getAllDominos()) {
+            if (domino.getId() == id) {
+                return domino;
+            }
+        }
+        throw new java.lang.IllegalArgumentException("Domino with ID " + id + " not found.");
+    }
+    private void createAllDominoes(Game game) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/alldominoes.dat"));
+            String line = "";
+            String delimiters = "[:\\+()]";
+            while ((line = br.readLine()) != null) {
+                String[] dominoString = line.split(delimiters); // {id, leftTerrain, rightTerrain, crowns}
+                int dominoId = Integer.decode(dominoString[0]);
+                TerrainType leftTerrain = getTerrainType(dominoString[1]);
+                TerrainType rightTerrain = getTerrainType(dominoString[2]);
+                int numCrown = 0;
+                if (dominoString.length > 3) {
+                    numCrown = Integer.decode(dominoString[3]);
+                }
+                new Domino(dominoId, leftTerrain, rightTerrain, numCrown, game);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new java.lang.IllegalArgumentException(
+                    "Error occured while trying to read alldominoes.dat: " + e.getMessage());
+        }
+    }
+
+    private TerrainType getTerrainType(String terrain) {
+        switch (terrain) {
+            case "W":
+                return TerrainType.WheatField;
+            case "F":
+                return TerrainType.Forest;
+            case "M":
+                return TerrainType.Mountain;
+            case "G":
+                return TerrainType.Grass;
+            case "S":
+                return TerrainType.Swamp;
+            case "L":
+                return TerrainType.Lake;
+            default:
+                throw new java.lang.IllegalArgumentException("Invalid terrain type: " + terrain);
+        }
+    }
+
+    private DominoInKingdom.DirectionKind getDirection(String dir) {
+        switch (dir) {
+            case "up":
+                return DominoInKingdom.DirectionKind.Up;
+            case "down":
+                return DominoInKingdom.DirectionKind.Down;
+            case "left":
+                return DominoInKingdom.DirectionKind.Left;
+            case "right":
+                return DominoInKingdom.DirectionKind.Right;
+            default:
+                throw new java.lang.IllegalArgumentException("Invalid direction: " + dir);
+        }
+    }
+
+    private Castle getCastle (Kingdom kingdom) {
+        for(KingdomTerritory territory: kingdom.getTerritories()){
+            if(territory instanceof Castle )
+                return (Castle)territory;
+        }
+        return null;
+    }
+
+}
