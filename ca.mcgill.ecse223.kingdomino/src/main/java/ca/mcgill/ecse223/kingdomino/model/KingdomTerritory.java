7544e433cca58945fc8b1f11bb53079f32f50161
violetwei <zhenzhou.wei@mail.mcgill.ca>
2020-03-05 14:38:08 -0500

    code refactor

--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/model/KingdomTerritory.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/model/KingdomTerritory.java
@@ -4,8 +4,7 @@
 package ca.mcgill.ecse223.kingdomino.model;
 
 // line 62 "../../../../../Kingdomino.ump"
-public abstract class KingdomTerritory
-{
+public abstract class KingdomTerritory {
 
   //------------------------
   // MEMBER VARIABLES
@@ -22,38 +21,30 @@ public abstract class KingdomTerritory
   // CONSTRUCTOR
   //------------------------
 
-  public KingdomTerritory(int aX, int aY, Kingdom aKingdom)
-  {
+  public KingdomTerritory(int aX, int aY, Kingdom aKingdom) {
     x = aX;
     y = aY;
     boolean didAddKingdom = setKingdom(aKingdom);
-    if (!didAddKingdom)
-    {
+    if (!didAddKingdom) {
       throw new RuntimeException("Unable to create territory due to kingdom");
     }
-    if (aX<=-5||aX>=5)
-    {
-      throw new RuntimeException("Please provide a valid x [x>-5&&x<5]");
+    if (aX<=-5||aX>=5) {
+      throw new RuntimeException("Please provide a valid x [x>-5 && x<5]");
     }
-    if (aY<=-5||aY>=5)
-    {
-      throw new RuntimeException("Please provide a valid y [y>-5&&y<5]");
+    if (aY<=-5||aY>=5) {
+      throw new RuntimeException("Please provide a valid y [y>-5 && y<5]");
     }
-    if (aX<=-5||aX>=5)
-    {
-      throw new RuntimeException("Please provide a valid x [x>-5&&x<5]");
+    if (aX<=-5||aX>=5) {
+      throw new RuntimeException("Please provide a valid x [x>-5 && x<5]");
     }
-    if (aY<=-5||aY>=5)
-    {
-      throw new RuntimeException("Please provide a valid y [y>-5&&y<5]");
+    if (aY<=-5||aY>=5) {
+      throw new RuntimeException("Please provide a valid y [y>-5 && y<5]");
     }
-    if (aX<=-5||aX>=5)
-    {
-      throw new RuntimeException("Please provide a valid x [x>-5&&x<5]");
+    if (aX<=-5||aX>=5) {
+      throw new RuntimeException("Please provide a valid x [x>-5 && x<5]");
     }
-    if (aY<=-5||aY>=5)
-    {
-      throw new RuntimeException("Please provide a valid y [y>-5&&y<5]");
+    if (aY<=-5||aY>=5) {
+      throw new RuntimeException("Please provide a valid y [y>-5 && y<5]");
     }
   }
 
@@ -61,36 +52,20 @@ public abstract class KingdomTerritory
   // INTERFACE
   //------------------------
 
-  public boolean setX(int aX)
-  {
+  public boolean setX(int aX) {
     boolean wasSet = false;
-    if (aX>-5&&aX<5)
-    {
-    if (aX>-5&&aX<5)
-    {
-    if (aX>-5&&aX<5)
-    {
-    x = aX;
-    wasSet = true;
-    }
-    }
+    if (aX>-5 && aX<5) {
+      x = aX;
+      wasSet = true;
     }
     return wasSet;
   }
 
-  public boolean setY(int aY)
-  {
+  public boolean setY(int aY) {
     boolean wasSet = false;
-    if (aY>-5&&aY<5)
-    {
-    if (aY>-5&&aY<5)
-    {
-    if (aY>-5&&aY<5)
-    {
-    y = aY;
-    wasSet = true;
-    }
-    }
+    if (aY>-5 && aY<5) {
+      y = aY;
+      wasSet = true;
     }
     return wasSet;
   }
@@ -98,33 +73,28 @@ public abstract class KingdomTerritory
   /**
    * The position defines the coordinates of the left tile of a domino or the castle itself
    */
-  public int getX()
-  {
+  public int getX() {
     return x;
   }
 
-  public int getY()
-  {
+  public int getY() {
     return y;
   }
+
   /* Code from template association_GetOne */
-  public Kingdom getKingdom()
-  {
+  public Kingdom getKingdom() {
     return kingdom;
   }
+
   /* Code from template association_SetOneToMany */
-  public boolean setKingdom(Kingdom aKingdom)
-  {
+  public boolean setKingdom(Kingdom aKingdom) {
     boolean wasSet = false;
-    if (aKingdom == null)
-    {
+    if (aKingdom == null) {
       return wasSet;
     }
-
     Kingdom existingKingdom = kingdom;
     kingdom = aKingdom;
-    if (existingKingdom != null && !existingKingdom.equals(aKingdom))
-    {
+    if (existingKingdom != null && !existingKingdom.equals(aKingdom)) {
       existingKingdom.removeTerritory(this);
     }
     kingdom.addTerritory(this);
@@ -132,19 +102,15 @@ public abstract class KingdomTerritory
     return wasSet;
   }
 
-  public void delete()
-  {
+  public void delete() {
     Kingdom placeholderKingdom = kingdom;
     this.kingdom = null;
-    if(placeholderKingdom != null)
-    {
+    if(placeholderKingdom != null) {
       placeholderKingdom.removeTerritory(this);
     }
   }
 
-
-  public String toString()
-  {
+  public String toString() {
     return super.toString() + "["+
             "x" + ":" + getX()+ "," +
             "y" + ":" + getY()+ "]" + System.getProperties().getProperty("line.separator") +
--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/model/Kingdomino.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/model/Kingdomino.java
@@ -5,8 +5,7 @@ package ca.mcgill.ecse223.kingdomino.model;
 import java.util.*;
 
 // line 5 "../../../../../Kingdomino.ump"
-public class Kingdomino
-{
+public class Kingdomino {
 
   //------------------------
   // MEMBER VARIABLES
@@ -22,8 +21,7 @@ public class Kingdomino
   // CONSTRUCTOR
   //------------------------
 
-  public Kingdomino()
-  {
+  public Kingdomino() {
     users = new ArrayList<User>();
     bonusOptions = new ArrayList<BonusOption>();
     allGames = new ArrayList<Game>();
@@ -33,152 +31,133 @@ public class Kingdomino
   // INTERFACE
   //------------------------
   /* Code from template association_GetMany */
-  public User getUser(int index)
-  {
+  public User getUser(int index) {
     User aUser = users.get(index);
     return aUser;
   }
 
-  public List<User> getUsers()
-  {
+  public List<User> getUsers() {
     List<User> newUsers = Collections.unmodifiableList(users);
     return newUsers;
   }
 
-  public int numberOfUsers()
-  {
+  public int numberOfUsers() {
     int number = users.size();
     return number;
   }
 
-  public boolean hasUsers()
-  {
+  public boolean hasUsers() {
     boolean has = users.size() > 0;
     return has;
   }
 
-  public int indexOfUser(User aUser)
-  {
+  public int indexOfUser(User aUser) {
     int index = users.indexOf(aUser);
     return index;
   }
+
   /* Code from template association_GetMany */
-  public BonusOption getBonusOption(int index)
-  {
+  public BonusOption getBonusOption(int index) {
     BonusOption aBonusOption = bonusOptions.get(index);
     return aBonusOption;
   }
 
-  public List<BonusOption> getBonusOptions()
-  {
+  public List<BonusOption> getBonusOptions() {
     List<BonusOption> newBonusOptions = Collections.unmodifiableList(bonusOptions);
     return newBonusOptions;
   }
 
-  public int numberOfBonusOptions()
-  {
+  public int numberOfBonusOptions() {
     int number = bonusOptions.size();
     return number;
   }
 
-  public boolean hasBonusOptions()
-  {
+  public boolean hasBonusOptions() {
     boolean has = bonusOptions.size() > 0;
     return has;
   }
 
-  public int indexOfBonusOption(BonusOption aBonusOption)
-  {
+  public int indexOfBonusOption(BonusOption aBonusOption) {
     int index = bonusOptions.indexOf(aBonusOption);
     return index;
   }
+
   /* Code from template association_GetMany */
-  public Game getAllGame(int index)
-  {
+  public Game getAllGame(int index) {
     Game aAllGame = allGames.get(index);
     return aAllGame;
   }
 
-  public List<Game> getAllGames()
-  {
+  public List<Game> getAllGames() {
     List<Game> newAllGames = Collections.unmodifiableList(allGames);
     return newAllGames;
   }
 
-  public int numberOfAllGames()
-  {
+  public int numberOfAllGames() {
     int number = allGames.size();
     return number;
   }
 
-  public boolean hasAllGames()
-  {
+  public boolean hasAllGames() {
     boolean has = allGames.size() > 0;
     return has;
   }
 
-  public int indexOfAllGame(Game aAllGame)
-  {
+  public int indexOfAllGame(Game aAllGame) {
     int index = allGames.indexOf(aAllGame);
     return index;
   }
+
   /* Code from template association_GetOne */
-  public Game getCurrentGame()
-  {
+  public Game getCurrentGame() {
     return currentGame;
   }
 
-  public boolean hasCurrentGame()
-  {
+  public boolean hasCurrentGame() {
     boolean has = currentGame != null;
     return has;
   }
+
   /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfUsers()
-  {
+  public static int minimumNumberOfUsers() {
     return 0;
   }
+
   /* Code from template association_AddManyToOne */
-  public User addUser(String aName)
-  {
+  public User addUser(String aName) {
     return new User(aName, this);
   }
 
-  public boolean addUser(User aUser)
-  {
+  public boolean addUser(User aUser) {
     boolean wasAdded = false;
-    if (users.contains(aUser)) { return false; }
+    if (users.contains(aUser)) { 
+      return false; 
+    }
     Kingdomino existingKingdomino = aUser.getKingdomino();
     boolean isNewKingdomino = existingKingdomino != null && !this.equals(existingKingdomino);
-    if (isNewKingdomino)
-    {
+    if (isNewKingdomino) {
       aUser.setKingdomino(this);
-    }
-    else
-    {
+    } else {
       users.add(aUser);
     }
     wasAdded = true;
     return wasAdded;
   }
 
-  public boolean removeUser(User aUser)
-  {
+  public boolean removeUser(User aUser) {
     boolean wasRemoved = false;
     //Unable to remove aUser, as it must always have a kingdomino
-    if (!this.equals(aUser.getKingdomino()))
-    {
+    if (!this.equals(aUser.getKingdomino())) {
       users.remove(aUser);
       wasRemoved = true;
     }
     return wasRemoved;
   }
+
   /* Code from template association_AddIndexControlFunctions */
-  public boolean addUserAt(User aUser, int index)
-  {  
+  public boolean addUserAt(User aUser, int index) {  
     boolean wasAdded = false;
-    if(addUser(aUser))
-    {
+    if(addUser(aUser)) {
       if(index < 0 ) { index = 0; }
       if(index > numberOfUsers()) { index = numberOfUsers() - 1; }
       users.remove(aUser);
@@ -188,69 +167,58 @@ public class Kingdomino
     return wasAdded;
   }
 
-  public boolean addOrMoveUserAt(User aUser, int index)
-  {
+  public boolean addOrMoveUserAt(User aUser, int index) {
     boolean wasAdded = false;
-    if(users.contains(aUser))
-    {
+    if(users.contains(aUser)) {
       if(index < 0 ) { index = 0; }
       if(index > numberOfUsers()) { index = numberOfUsers() - 1; }
       users.remove(aUser);
       users.add(index, aUser);
       wasAdded = true;
-    } 
-    else 
-    {
+    } else {
       wasAdded = addUserAt(aUser, index);
     }
     return wasAdded;
   }
+
   /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfBonusOptions()
-  {
+  public static int minimumNumberOfBonusOptions() {
     return 0;
   }
+
   /* Code from template association_AddManyToOne */
-  public BonusOption addBonusOption(String aOptionName)
-  {
+  public BonusOption addBonusOption(String aOptionName) {
     return new BonusOption(aOptionName, this);
   }
 
-  public boolean addBonusOption(BonusOption aBonusOption)
-  {
+  public boolean addBonusOption(BonusOption aBonusOption) {
     boolean wasAdded = false;
     if (bonusOptions.contains(aBonusOption)) { return false; }
     Kingdomino existingKingdomino = aBonusOption.getKingdomino();
     boolean isNewKingdomino = existingKingdomino != null && !this.equals(existingKingdomino);
-    if (isNewKingdomino)
-    {
+    if (isNewKingdomino) {
       aBonusOption.setKingdomino(this);
-    }
-    else
-    {
+    } else {
       bonusOptions.add(aBonusOption);
     }
     wasAdded = true;
     return wasAdded;
   }
 
-  public boolean removeBonusOption(BonusOption aBonusOption)
-  {
+  public boolean removeBonusOption(BonusOption aBonusOption) {
     boolean wasRemoved = false;
     //Unable to remove aBonusOption, as it must always have a kingdomino
-    if (!this.equals(aBonusOption.getKingdomino()))
-    {
+    if (!this.equals(aBonusOption.getKingdomino())) {
       bonusOptions.remove(aBonusOption);
       wasRemoved = true;
     }
     return wasRemoved;
   }
+
   /* Code from template association_AddIndexControlFunctions */
-  public boolean addBonusOptionAt(BonusOption aBonusOption, int index)
-  {  
+  public boolean addBonusOptionAt(BonusOption aBonusOption, int index) {  
     boolean wasAdded = false;
-    if(addBonusOption(aBonusOption))
-    {
+    if(addBonusOption(aBonusOption)) {
       if(index < 0 ) { index = 0; }
       if(index > numberOfBonusOptions()) { index = numberOfBonusOptions() - 1; }
       bonusOptions.remove(aBonusOption);
@@ -260,69 +228,58 @@ public class Kingdomino
     return wasAdded;
   }
 
-  public boolean addOrMoveBonusOptionAt(BonusOption aBonusOption, int index)
-  {
+  public boolean addOrMoveBonusOptionAt(BonusOption aBonusOption, int index) {
     boolean wasAdded = false;
-    if(bonusOptions.contains(aBonusOption))
-    {
+    if(bonusOptions.contains(aBonusOption)) {
       if(index < 0 ) { index = 0; }
       if(index > numberOfBonusOptions()) { index = numberOfBonusOptions() - 1; }
       bonusOptions.remove(aBonusOption);
       bonusOptions.add(index, aBonusOption);
       wasAdded = true;
-    } 
-    else 
-    {
+    } else {
       wasAdded = addBonusOptionAt(aBonusOption, index);
     }
     return wasAdded;
   }
+
   /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfAllGames()
-  {
+  public static int minimumNumberOfAllGames() {
     return 0;
   }
+
   /* Code from template association_AddManyToOne */
-  public Game addAllGame(int aMaxPileSize)
-  {
+  public Game addAllGame(int aMaxPileSize) {
     return new Game(aMaxPileSize, this);
   }
 
-  public boolean addAllGame(Game aAllGame)
-  {
+  public boolean addAllGame(Game aAllGame) {
     boolean wasAdded = false;
     if (allGames.contains(aAllGame)) { return false; }
     Kingdomino existingKingdomino = aAllGame.getKingdomino();
     boolean isNewKingdomino = existingKingdomino != null && !this.equals(existingKingdomino);
-    if (isNewKingdomino)
-    {
+    if (isNewKingdomino) {
       aAllGame.setKingdomino(this);
-    }
-    else
-    {
+    } else {
       allGames.add(aAllGame);
     }
     wasAdded = true;
     return wasAdded;
   }
 
-  public boolean removeAllGame(Game aAllGame)
-  {
+  public boolean removeAllGame(Game aAllGame) {
     boolean wasRemoved = false;
     //Unable to remove aAllGame, as it must always have a kingdomino
-    if (!this.equals(aAllGame.getKingdomino()))
-    {
+    if (!this.equals(aAllGame.getKingdomino())) {
       allGames.remove(aAllGame);
       wasRemoved = true;
     }
     return wasRemoved;
   }
+
   /* Code from template association_AddIndexControlFunctions */
-  public boolean addAllGameAt(Game aAllGame, int index)
-  {  
+  public boolean addAllGameAt(Game aAllGame, int index) {  
     boolean wasAdded = false;
-    if(addAllGame(aAllGame))
-    {
+    if(addAllGame(aAllGame)) {
       if(index < 0 ) { index = 0; }
       if(index > numberOfAllGames()) { index = numberOfAllGames() - 1; }
       allGames.remove(aAllGame);
@@ -332,50 +289,40 @@ public class Kingdomino
     return wasAdded;
   }
 
-  public boolean addOrMoveAllGameAt(Game aAllGame, int index)
-  {
+  public boolean addOrMoveAllGameAt(Game aAllGame, int index) {
     boolean wasAdded = false;
-    if(allGames.contains(aAllGame))
-    {
+    if(allGames.contains(aAllGame)) {
       if(index < 0 ) { index = 0; }
       if(index > numberOfAllGames()) { index = numberOfAllGames() - 1; }
       allGames.remove(aAllGame);
       allGames.add(index, aAllGame);
       wasAdded = true;
-    } 
-    else 
-    {
+    } else {
       wasAdded = addAllGameAt(aAllGame, index);
     }
     return wasAdded;
   }
+
   /* Code from template association_SetUnidirectionalOptionalOne */
-  public boolean setCurrentGame(Game aNewCurrentGame)
-  {
+  public boolean setCurrentGame(Game aNewCurrentGame) {
     boolean wasSet = false;
     currentGame = aNewCurrentGame;
     wasSet = true;
     return wasSet;
   }
 
-  public void delete()
-  {
-    while (users.size() > 0)
-    {
+  public void delete() {
+    while (users.size() > 0) {
       User aUser = users.get(users.size() - 1);
       aUser.delete();
       users.remove(aUser);
     }
-    
-    while (bonusOptions.size() > 0)
-    {
+    while (bonusOptions.size() > 0) {
       BonusOption aBonusOption = bonusOptions.get(bonusOptions.size() - 1);
       aBonusOption.delete();
       bonusOptions.remove(aBonusOption);
     }
-    
-    while (allGames.size() > 0)
-    {
+    while (allGames.size() > 0) {
       Game aAllGame = allGames.get(allGames.size() - 1);
       aAllGame.delete();
       allGames.remove(aAllGame);
