bf2511ff1384df2b98a3e491c0db9210e9e5c355
ZeroLifeCat <mandylogan8x@gmail.com>
2020-03-09 02:44:19 +0800

    Identify Property Feature Passed

---
+++ ca.mcgill.ecse223.kingdomino/src/test/java/ca/mcgill/ecse223/kingdomino/features/BrowseDominoPileStepDefinition.java
@@ -0,0 +1,124 @@
+package ca.mcgill.ecse223.kingdomino.features;
+
+import ca.mcgill.ecse223.kingdomino.KingdominoApplication;
+import ca.mcgill.ecse223.kingdomino.model.*;
+import io.cucumber.java.en.Given;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BrowseDominoPileStepDefinition {
+    @Given("the program is started and ready for browsing dominoes")
+    public void the_program_is_started_and_ready_for_browsing_dominoes(){
+
+    }
+
+    ///////////////////////////////////////
+    /// -----Private Helper Methods---- ///
+    ///////////////////////////////////////
+
+    private void addDefaultUsersAndPlayers(Game game) {
+        String[] userNames = { "User1", "User2", "User3", "User4" };
+        for (int i = 0; i < userNames.length; i++) {
+            User user = game.getKingdomino().addUser(userNames[i]);
+            Player player = new Player(game);
+            player.setUser(user);
+            player.setColor(Player.PlayerColor.values()[i]);
+            Kingdom kingdom = new Kingdom(player);
+            new Castle(0, 0, kingdom, player);
+        }
+    }
+
+    private void createAllDominoes(Game game) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/alldominoes.dat"));
+            String line = "";
+            String delimiters = "[:\\+()]";
+            while ((line = br.readLine()) != null) {
+                String[] dominoString = line.split(delimiters); // {id, leftTerrain, rightTerrain, crowns}
+                int dominoId = Integer.decode(dominoString[0]);
+                TerrainType leftTerrain = getTerrainType(dominoString[1]);
+                TerrainType rightTerrain = getTerrainType(dominoString[2]);
+                int numCrown = 0;
+                if (dominoString.length > 3) {
+                    numCrown = Integer.decode(dominoString[3]);
+                }
+                new Domino(dominoId, leftTerrain, rightTerrain, numCrown, game);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new java.lang.IllegalArgumentException(
+                    "Error occured while trying to read alldominoes.dat: " + e.getMessage());
+        }
+    }
+
+    private Domino getdominoByID(int id) {
+        Game game = KingdominoApplication.getKingdomino().getCurrentGame();
+        for (Domino domino : game.getAllDominos()) {
+            if (domino.getId() == id) {
+                return domino;
+            }
+        }
+        throw new java.lang.IllegalArgumentException("Domino with ID " + id + " not found.");
+    }
+
+    private TerrainType getTerrainType(String terrain) {
+        switch (terrain) {
+            case "W":
+                return TerrainType.WheatField;
+            case "F":
+                return TerrainType.Forest;
+            case "M":
+                return TerrainType.Mountain;
+            case "G":
+                return TerrainType.Grass;
+            case "S":
+                return TerrainType.Swamp;
+            case "L":
+                return TerrainType.Lake;
+            default:
+                throw new java.lang.IllegalArgumentException("Invalid terrain type: " + terrain);
+        }
+    }
+
+    private DominoInKingdom.DirectionKind getDirection(String dir) {
+        switch (dir) {
+            case "up":
+                return DominoInKingdom.DirectionKind.Up;
+            case "down":
+                return DominoInKingdom.DirectionKind.Down;
+            case "left":
+                return DominoInKingdom.DirectionKind.Left;
+            case "right":
+                return DominoInKingdom.DirectionKind.Right;
+            default:
+                throw new java.lang.IllegalArgumentException("Invalid direction: " + dir);
+        }
+    }
+
+    private Domino.DominoStatus getDominoStatus(String status) {
+        switch (status) {
+            case "inPile":
+                return Domino.DominoStatus.InPile;
+            case "excluded":
+                return Domino.DominoStatus.Excluded;
+            case "inCurrentDraft":
+                return Domino.DominoStatus.InCurrentDraft;
+            case "inNextDraft":
+                return Domino.DominoStatus.InNextDraft;
+            case "erroneouslyPreplaced":
+                return Domino.DominoStatus.ErroneouslyPreplaced;
+            case "correctlyPreplaced":
+                return Domino.DominoStatus.CorrectlyPreplaced;
+            case "placedInKingdom":
+                return Domino.DominoStatus.PlacedInKingdom;
+            case "discarded":
+                return Domino.DominoStatus.Discarded;
+            default:
+                throw new java.lang.IllegalArgumentException("Invalid domino status: " + status);
+        }
+    }
+}
+
